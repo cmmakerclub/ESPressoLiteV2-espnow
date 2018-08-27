8d4d03a4c854ede214a28f2972f872c6e53af571
Nat <nat@cmmc.io>
2018-08-27 17:47:20 +0700

    add utils.

--- src/modules/SensorModule.cpp
+++ src/modules/SensorModule.cpp
@@ -2,8 +2,63 @@
 
 #define NUMPIXELS      7
 
+void swap(float &a, float &b)
+{
+  int t = a;
+  a = b;
+  b = t;
+}
 
-void SensorModule::config(CMMC_System *os, AsyncWebServer* server) {
+int partition(float *arr, const int left, const int right)
+{
+  const int mid = left + (right - left) / 2;
+  const int pivot = arr[mid];
+  // move the mid point value to the front.
+  swap(arr[mid], arr[left]);
+  int i = left + 1;
+  int j = right;
+  while (i <= j)
+  {
+    while (i <= j && arr[i] <= pivot)
+    {
+      i++;
+    }
+
+    while (i <= j && arr[j] > pivot)
+    {
+      j--;
+    }
+
+    if (i < j)
+    {
+      swap(arr[i], arr[j]);
+    }
+  }
+
+  swap(arr[i - 1], arr[left]);
+  return i - 1;
+}
+
+void quickSort(float *arr, const int left, const int right)
+{
+  if (left >= right)
+  {
+    return;
+  }
+
+  int part = partition(arr, left, right);
+
+  quickSort(arr, left, part - 1);
+  quickSort(arr, part + 1, right);
+}
+
+int median(float arr[], int maxValues)
+{
+  quickSort(arr, 0, maxValues - 1);
+  return arr[maxValues / 2];
+}
+
+void SensorModule::config(CMMC_System *os, AsyncWebServer* server) { 
   static SensorModule *that = this;
   pixels  = new Adafruit_NeoPixel(NUMPIXELS, 15, NEO_GRB + NEO_KHZ800);
   oled  = new SSD1306(0x3c, 4, 5);
